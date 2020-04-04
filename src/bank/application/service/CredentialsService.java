2e1463028778078b96dd4f47645e3a93bc410f7f
andreiherdes <andrei.herdes@info.uaic.ro>
2020-04-04 20:00:14 +0300

    Added more details to the credentialsService task

--- src/bank/application/service/CredentialsService.java
+++ src/bank/application/service/CredentialsService.java
@@ -8,7 +8,8 @@ public class CredentialsService {
     //Constructor
 
     public boolean authenticateUser(AuthenticationModel credentials) {
-        // Implement a way in which you check if the username/password combination is found in the credentialsFile
+        // Implement a way in which you check if the username/password combination is found in the credentials file
+        // you need to read data from the file and parse the username-password, you can find an example in App.main method
         // this methods will return true if the username/password matched an entry from the file
         // false otherwise
         return false;
